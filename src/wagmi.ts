646d36ff0a63d68685317faac94c213153e54fa0
Jane <m13221001019@163.com>
2023-03-28 20:31:12 +0800

    init wagmi

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -3,8 +3,10 @@ import { configureChains, createClient } from 'wagmi'
 import { Chain, goerli, mainnet, arbitrum, arbitrumGoerli, polygon, polygonMumbai, bsc, bscTestnet, iotex, iotexTestnet } from 'wagmi/chains'
 import { publicProvider } from 'wagmi/providers/public'
 
+// @ts-ignore
 const defaultChains: Chain[] = [mainnet, bsc, {...iotex, iconUrl: 'https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@1a63530be6e374711a8554f31b17e4cb92c25fa5/svg/icon/iotx.svg'}, arbitrum, polygon];
 
+// @ts-ignore
 const defaultTestChains: Chain[] = [goerli, bscTestnet, {...iotexTestnet, iconUrl: 'https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@1a63530be6e374711a8554f31b17e4cb92c25fa5/svg/icon/iotx.svg'}, polygonMumbai, arbitrumGoerli];
 
 const { chains, provider, webSocketProvider } = configureChains(
